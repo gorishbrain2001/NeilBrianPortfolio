cac59b01be44ae8e0494d6e4a57cc4612944f770
gorishbrain2001 <neilbriangore@gmail.com>
2024-05-23 14:33:42 +0800

    assets

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+$(function() {
+  // This will select everything with the class smooth-scroll
+  $('.smooth-scroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 800); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
+});
+
+// Change the speed to whatever you want
+// Personally i think 1000 is too much
+// Try 800 or below, it seems not too much but it will make a difference
+
+window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+	    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	        document.getElementById("navbar").style.top = "0";
+	    } else {
+	        document.getElementById("navbar").style.top = "-100px";
+	    }
+	}
+
+// var i = 0;
+// var txt = 'Aldrin Manahan';
+// var speed = 500;
+
+// window.onload = function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("demo").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+

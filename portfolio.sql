dffb88968ad5b22b61a86233ed5c9f9d172e4ab0
gorishbrain2001 <neilbriangore@gmail.com>
2024-05-23 14:40:08 +0800

    portfolio

---
+++ portfolio.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `contact_us` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `message` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4
